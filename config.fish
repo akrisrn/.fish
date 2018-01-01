906cf769b2c7935851075aaa4fe7c91d8fe07a2d
AkrISrn <shairkarine@gmail.com>
2018-01-02 00:22:26 +0800

    添加WSL用默认打开TMUX配置

--- config.fish
+++ config.fish
@@ -7,3 +7,16 @@ fundle init
 
 set proxy_host 127.0.0.1:1080
 set proxy_auth false
+
+if test -z "$TMUX"; and test -n "$USE_TMUX"
+    if test -n "$ATTACH_ONLY"
+        if tmux a 2>/dev/null
+        else
+            cd; and exec tmux
+        end
+        exit
+    end
+
+    tmux new-window -c "$PWD" 2>/dev/null; and exec tmux a
+    exec tmux
+end

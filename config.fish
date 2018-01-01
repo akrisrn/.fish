dd94529458764cdc716a0feeb29060e9af4ad034
AkrISrn <shairkarine@gmail.com>
2018-01-01 20:40:37 +0800

    first commit

---
+++ config.fish
@@ -0,0 +1,7 @@
+fundle plugin 'oh-my-fish/theme-bobthefish'
+fundle plugin 'oh-my-fish/plugin-proxy'
+fundle plugin 'oh-my-fish/plugin-hash'
+fundle init
+
+set proxy_host 127.0.0.1:1080
+set proxy_auth false

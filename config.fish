2ada74a2c54669cef92b89f3c0b274cde5ea4b54
AkrISrn <shairkarine@gmail.com>
2018-01-01 20:55:36 +0800

    添加oh—my-fish-core插件

--- config.fish
+++ config.fish
@@ -1,6 +1,7 @@
 fundle plugin 'oh-my-fish/theme-bobthefish'
 fundle plugin 'oh-my-fish/plugin-proxy'
 fundle plugin 'oh-my-fish/plugin-hash'
+fundle plugin 'tuvistavie/oh-my-fish-core'
 fundle init
 
 set proxy_host 127.0.0.1:1080

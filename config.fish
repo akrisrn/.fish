f1e0170da36e588f3ec886f622712a89b8eb866e
AkrISrn <shairkarine@gmail.com>
2018-01-02 09:14:28 +0800

    转移bash相关配置

--- config.fish
+++ config.fish
@@ -7,6 +7,14 @@ fundle init
 
 set -x LANG zh_CN.UTF-8
 
+set -x alicecs 120.77.156.58
+set -x knowledgecs 47.74.2.154
+set -x li158835 139.162.104.35
+
+alias ll='ls -alF'
+alias la='ls -A'
+alias l='ls -CF'
+
 set proxy_host 127.0.0.1:1080
 set proxy_auth false
 
